b58546d3925ee13e71877e2a4dfc0b8a6a0679a0
jeff <jfroussel75@gmail.com>
2017-11-05 23:57:42 +0100

    WIP fullcalendar component

--- src/routes/app/routes/corporate/routes/calendars/components/Calendars.js
+++ src/routes/app/routes/corporate/routes/calendars/components/Calendars.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'fullcalendar-reactwrapper/dist/css/fullcalendar.min.css';
 import FullCalendar from 'fullcalendar-reactwrapper';
+import './fullcalendarCustom.css';
 import events from './events';
 
 class Calendar extends React.Component {
@@ -19,13 +20,15 @@ class Calendar extends React.Component {
           header = {{
             left: 'prev,next today myCustomButton',
             center: 'title',
-            right: 'month,basicWeek,basicDay'
+            right: 'prevYear,month,basicWeek,basicDay,nextYear,close',
           }}
+          footer={{right:'nextYear'}}
           defaultDate={'2017-09-12'}
           navLinks= {true}
           editable= {true}
           eventLimit= {true}
           events = {this.state.events}
+          locale={'fr'}
         />
       </div>
     );
